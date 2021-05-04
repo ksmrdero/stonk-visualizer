24c4bd5c8675761603dd0b0c34850a2b2a3e390a
James Wei <jameswei2000@gmail.com>
2021-05-03 22:17:19 -0500

    shit works

--- scatter.js
+++ scatter.js
@@ -4,7 +4,7 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
   height = 400 - margin.top - margin.bottom,
   x_pad = 0.15,
   curr_tweet = "elonmusk";
-  curr_stock = "DOGE_returns",
+  curr_stock = "GME_returns",
 
 
 
@@ -110,6 +110,46 @@ function draw(tweet, stock){
     //   .domain(["setosa", "versicolor", "virginica"])
     //   .range(["#F8766D", "#00BA38", "#619CFF"])
 
+
+
+    // .style("fill", function (d) { return color(d.Species) })
+    var tooltip = d3.select("#tweettxt")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "10px")
+      
+
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    var mouseover = function (d) {
+      console.log("hitt")
+      tooltip
+        .style("opacity", 1)
+      
+      var f = d3.select("#tweettxt").style("display", "block")
+    }
+    var mousemove = function (d) {
+      console.log('hit');
+      tooltip
+        .html(d.txt)
+        .style("left", (d3.mouse(this)[0] + 10) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+        .style("top", (d3.mouse(this)[1]) + "px")
+  
+    }
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    var mouseleave = function (d) {
+      tooltip
+        .transition()
+        .duration(200)
+        // .style("opacity", 0)
+    }
     // Add dots
     svg.append('g')
       .selectAll("dot")
@@ -119,7 +159,25 @@ function draw(tweet, stock){
       .attr("cx", function (d) { return x(d.sentiment); })
       .attr("cy", function (d) { return y(d[stock]); })
       .attr("r", 5)
-    // .style("fill", function (d) { return color(d.Species) })
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+    // // Add dots
+    // svg.append('g')
+    //   .selectAll("dot")
+    //   .data(data.filter(function (d, i) { return i < 50 })) // the .filter part is just to keep a few dots on the chart, not all of them
+    //   .enter()
+    //   .append("circle")
+    //   .attr("cx", function (d) { return x(d.GrLivArea); })
+    //   .attr("cy", function (d) { return y(d.SalePrice); })
+    //   .attr("r", 7)
+    //   .style("fill", "#69b3a2")
+    //   .style("opacity", 0.3)
+    //   .style("stroke", "white")
+    //   .on("mouseover", mouseover)
+    //   .on("mousemove", mousemove)
+    //   .on("mouseleave", mouseleave)
+
 
   })
 
