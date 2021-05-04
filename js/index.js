540e5a3ee4367cbea5bde7469a1d22fd8d9efaf6
Eric McCarthy <ericm4@illinois.edu>
2021-05-04 09:30:13 -0500

    basically done

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
   height = 400 - margin.top - margin.bottom,
   pad = 1,
   curr_stock = "GME",
-  curr_sub = "wallstreetbets";
+  curr_sub = "wallstreetbets",
+  curr_tweets = "elonmusk";
 
-$("#instructions").show().delay(5000).fadeOut();
 // draw gme and wsb first
 draw(curr_stock, curr_sub)
 
@@ -17,19 +17,26 @@ function parseDate(date) {
 d3.selectAll("input[name='stock']").on("change", function () {
   $("#d3").empty();
   console.log(this.id);
-  draw(this.id, curr_sub);
+  draw(this.id, curr_sub, curr_tweets);
   curr_stock = this.id;
 });
 
 d3.selectAll("input[name='sentiment']").on("change", function () {
   $("#d3").empty();
   console.log(this.id);
-  draw(curr_stock, this.id);
+  draw(curr_stock, this.id, curr_tweets);
   curr_sub = this.id;
 });
 
+d3.selectAll("input[name='tweets']").on("change", function () {
+  $("#d3").empty();
+  console.log(this.id);
+  draw(curr_stock, curr_sub, this.id);
+  curr_tweets = this.id;
+});
 
-function draw(stock, sub) {
+
+function draw(stock, sub, tweets) {
   var svg = d3.select("#d3")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -166,8 +173,21 @@ function draw(stock, sub) {
         .attr("stroke-width", .8)
         .attr("d", line2);
 
-      // Rectangle covering graph to trigger mouse events
-      svg.append('rect')
+
+      // Handle Tweet data
+      d3.csv(`data/twitter/final_tweet_data_1week.csv`, 
+      function (d) {
+        if (d.user != curr_tweets) {
+          return;
+        }
+        var t = {date: d3.timeParse("%s")(d.timestamp), text: d.text, user: d.user, link: d.link};
+        return t;
+      },
+  
+      function (tweet_data) {
+        // console.log(tweet_data);
+        // rect covering graph to capture mouse events
+        svg.append('rect')
         .style("fill", "none")
         .style("pointer-events", "all")
         .attr('width', width)
@@ -176,90 +196,152 @@ function draw(stock, sub) {
         .on('mousemove', mousemove)
         .on('mouseout', mouseout);
 
-      function mouseover() {
-        focus.style("opacity", 1)
-        tooltipText.style("opacity", 1)
-        focus2.style("opacity", 1)
-        tooltipText2.style("opacity", 1)
-        focusDate.style("opacity", 1)
-      }
 
-      function mousemove() {
-        var x0 = x.invert(d3.mouse(this)[0]);
-        var i = bisect(data, x0, 1);
-        selectedData = data[i];
-
-
-      
-        focus
-          .attr("cx", x(selectedData.date))
-          .attr("cy", y(selectedData.value))
-        tooltipText
-          .html("Price Change: " + selectedData.value + "% ($" + selectedData.price + ")")
-          .attr("x", width-220)
-          .attr("y", 0)
-          .attr("font-size", "10px")
-
-         
-        focus2
-          .attr("cx", x(selectedData.date))
-          .attr("cy", y(selectedData.score))
-        tooltipText2
-          .html("Sentiment Change: " + selectedData.score + "% (" + selectedData.raw_score + ")")
-          .attr("x", width - 220)
-          .attr("y", 20)
-          .attr("font-size", "10px")
-
-        focusDate
-          .html("Date: " + parseDate(selectedData.date))
-          .attr("x",width-450)
-          .attr("y", height+20)
-          .attr("font-size", "14px")
+        var tooltip = d3.select("#tweettxt")
+        .append("div")
+        .style("opacity", 0)
+        .attr("id", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("font-size", "14px")
         
-      }
+        var mouseover1 = function (d) {
+          console.log("hit")
+          tooltip
+            .style("opacity", 1)
 
-      // Text disappears when mouse is out of frame
-      function mouseout() {
-        focus.style("opacity", 0)
-        tooltipText.style("opacity", 0)
-        focus2.style("opacity", 0)
-        tooltipText2.style("opacity", 0)
-        focusDate.style("opacity", 0)
-      }
+          var ref = d3.select("#link")
+            .attr("href", d.link)
 
-
-      //Zooming Functionality
-      svg.call(zoom);
-      function zoom(svg) {
-
-        var extent = [
-          [margin.left, margin.top],
-          [width - margin.right, height - margin.top]
-        ];
-
-        var zooming = d3.zoom()
-          .scaleExtent([1, 5])
-          .translateExtent(extent)
-          .extent(extent)
-          .on("zoom", zoomed);
-
-        svg.call(zooming);
-
-        function zoomed() {
-
-          x.range([margin.left, width - margin.right]
-            .map(d => d3.event.transform.applyX(d)));
-
-          svg.select("#line1")
-            .attr("d", line);
-          
-          svg.select("#line2")
-            .attr("d", line2);
-
-          svg.select(".x-axis")
-            .call(d3.axisBottom(x)
-              .tickSizeOuter(0));
+          d3.select(this).style("fill", "red")
         }
-      }
+        var mousemove1 = function (d) {
+          console.log('hit');
+          tooltip
+            .html("@" + d.user+ " "+ d.text)
+            .style("left", (d3.mouse(this)[0] + 10) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+            .style("top", (d3.mouse(this)[1]) + "px")
+        }
+        // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+        var mouseleave1 = function (d) {
+          tooltip
+            .transition()
+            .duration(200)
+            // .style("opacity", 0)
+          d3.select(this).style("fill", "#99e0e8")
+        }
+
+        // Add tweets
+        svg.append('g')
+        .selectAll("circle")
+        .data(tweet_data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { console.log(x(d.date)); return x(d.date); })
+        .attr("cy", function (d) { return y(10); })
+        .attr("r", 6)
+        .style("fill", "#99e0e8")
+        .on("mouseover", mouseover1)
+        .on("mousemove", mousemove1)
+        .on("mouseleave", mouseleave1)
+        // .on("click", function(d) {
+        //   // dom = document.getElementById('tweet-embed'),
+        //   // Promise.resolve().then(() => twttr.widgets.createTweet(id, dom));
+        //   // console.log('<a href="https://'+d.link+'"></a>');
+        //   // d3.select("#tweet-embed").html('<a href="https://'+d.link+'"></a>');
+        //   d3.select("#tweet-embed").text(d.text);
+
+
+        function mouseover() {
+          focus.style("opacity", 1)
+          tooltipText.style("opacity", 1)
+          focus2.style("opacity", 1)
+          tooltipText2.style("opacity", 1)
+          focusDate.style("opacity", 1)
+        }
+
+        function mousemove() {
+          var x0 = x.invert(d3.mouse(this)[0]);
+          var i = bisect(data, x0, 1);
+          selectedData = data[i];
+
+          focus
+            .attr("cx", x(selectedData.date))
+            .attr("cy", y(selectedData.value))
+          tooltipText
+            .html("Price Change: " + selectedData.value + "% ($" + selectedData.price + ")")
+            .attr("x", width-220)
+            .attr("y", 0)
+            .attr("font-size", "10px")
+          
+          focus2
+            .attr("cx", x(selectedData.date))
+            .attr("cy", y(selectedData.score))
+          tooltipText2
+            .html("Sentiment Change: " + selectedData.score + "% (" + selectedData.raw_score + ")")
+            .attr("x", width - 220)
+            .attr("y", 20)
+            .attr("font-size", "10px")
+
+          focusDate
+            .html("Date: " + parseDate(selectedData.date) + " CST")
+            .attr("x",width-450)
+            .attr("y", height+20)
+            .attr("font-size", "14px")
+          
+        }
+
+        // Text disappears when mouse is out of frame
+        function mouseout() {
+          focus.style("opacity", 0)
+          tooltipText.style("opacity", 0)
+          focus2.style("opacity", 0)
+          tooltipText2.style("opacity", 0)
+          focusDate.style("opacity", 0)
+        }
+
+        //Zooming Functionality
+        svg.call(zoom);
+        function zoom(svg) {
+
+          var extent = [
+            [margin.left, margin.top],
+            [width - margin.right, height - margin.top]
+          ];
+
+          var zooming = d3.zoom()
+            .scaleExtent([1, 5])
+            .translateExtent(extent)
+            .extent(extent)
+            .on("zoom", zoomed);
+
+          svg.call(zooming);
+
+          function zoomed() {
+
+            x.range([margin.left, width - margin.right]
+              .map(d => d3.event.transform.applyX(d)));
+
+            svg.select("#line1")
+              .attr("d", line);
+            
+            svg.select("#line2")
+              .attr("d", line2);
+
+            svg.select(".x-axis")
+              .call(d3.axisBottom(x)
+                .tickSizeOuter(0));
+                
+            svg.selectAll("circle")
+              .data(tweet_data)
+              .attr("cx", function (d) { console.log(x(d.date)); return x(d.date); })
+              .attr("cy", function (d) { return y(10); });
+          }
+        }
+      })
     })
+
 }
